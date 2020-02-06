0547c4fb335d51f4955b0c15f39400ef23a9e290
p00293412 <jakob.hendrix@la.gov>
2020-02-06 14:05:53 -0600

    Wired up MVVM using Caliburn and Micro

---
+++ ARMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using ARMDesktopUI.ViewModels;
+using Caliburn.Micro;
+
+namespace ARMDesktopUI
+{
+    /// <summary>
+    /// Used to set up Caliburn and Micro
+    /// </summary>
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // On Startup, launch ShellViewModel as our base. This will launch the view.
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ ARMDesktopUI/notes.md
@@ -0,0 +1,11 @@
+﻿# Markdown File
+
+## MVVM
+
+UI Layer
+
+* Views - display data
+* Models - data for views to display (not data access)
+* ViewModels - glue that holds Views and Models together. Takes the model and gives it to the view.
+
+Where do I put my data access? Not in this project.
