0a4638694099f11520a9a4be390108dca0973515
p00293412 <jakob.hendrix@la.gov>
2020-02-21 14:13:49 -0600

    Replace containter reference with IoC

--- ARMDesktopUI/ViewModels/ShellViewModel.cs
+++ ARMDesktopUI/ViewModels/ShellViewModel.cs
@@ -7,19 +7,17 @@ namespace ARMDesktopUI.ViewModels
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
         private IEventAggregator _events;
 
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
-            _container = container;
             _salesVM = salesVM;
 
             _events = events;
             _events.Subscribe(this);
 
             // each request gets a new instance. The login view will go away upon activation of a new item
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
